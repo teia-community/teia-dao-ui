79cdd9947d1254c9349954b7cd71875341aee54e
Javier Gracia Carpio <jagracar@gmail.com>
2023-08-02 20:22:39 +0200

    Update claim contract info

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const NETWORK = 'ghostnet';
 
 export const DAO_CONTRACT_ADDRESS = 'KT1PnfY7BdCKyu4bCb5Ufpidv6wYGsV2UfSt';
 
-export const TOKEN_DROP_CONTRACT_ADDRESS = 'KT1NhaMvktjR89DcdNw6gYbtW9nXnivn65uq';
+export const TOKEN_DROP_CONTRACT_ADDRESS = 'KT1C86QiA8gVzUQ2THPUQ42ZYWxok61Vo3wf';
 
 export const RPC_NODE = 'https://ghostnet.ecadinfra.com';
 
@@ -13,18 +13,18 @@ export const IPFS_GATEWAY = 'https://ipfs.io/ipfs/';
 
 export const TOKEN_DECIMALS = 1000000;
 
-export const DISTRIBUTION_MAPPING_IPFS_PATH = "QmXDnvkw5ZxNQKKtNaYyve5JuCeM8PfxRM9iAZCi84JKpN";
+export const DISTRIBUTION_MAPPING_IPFS_PATH = "QmbRmck8A5sBYQC7WEuK8dApnGQGXBhyPEgQpLm8ftfAtL";
 
 export const MERKLE_DATA_IPFS_PATHS = {
-    0: "QmR3tiFDi8vgTdvR98jS8jLn2No8EheMG6WTnpbzbupvZm",
-    1: "QmWG2jqiHXFk6Ub7xY7TtjYomxern3SNXuCDVxVeFc88iA",
-    2: "QmZ6P44rhTMxKHqADcW27quQPgvs6h1YTQaz1hS8J3FggF",
-    3: "Qmbm99hPZBQKg5wUhzYTuQUrit5eDgvytqU8J17kpcC71T",
-    4: "QmVBQjr7m25MgF2S7BkmNMtzVNfSuWCaxsYJMXkA7PNhKu",
-    5: "QmTi6E5HJJGZrwKkvBRYMHx2DaVD9mij9rm5cSyDVWv64f",
-    6: "QmbZsvCDiGv4xWeLtMf1wPxjogpvLjaKLwdZe5xQezUWUS",
-    7: "QmbCvvyP2s6nqq1cCW9H617jRGEYoZkV3SoJHCS9sJmsNU",
-    8: "QmRhr7m6zBtU1YFNcLMo34jQVUFdamG3BCGCFjA9AwBEnK"
+    0: "QmUETPfQaoE2mTzBUMbXGk4VgJUqvitwmti2iuk7p1KUPa",
+    1: "Qme3evVseBhLhoK4iefXiSHnr2wegGVnPccCYKQWqrDuPy",
+    2: "QmdGaYaM1gdqqVVaApmwxdg5Mu5Ckkh9hdDSfnC6nP6G14",
+    3: "QmZQzGdwZgMa8aNwhyFiLy2rKHmyvfFpi6pYfdE3t7wHTV",
+    4: "QmbB2LyNoVEy6jZTZ66GZL4ousBxGA1EeiEV5vCWyH2Wkq",
+    5: "QmPi7kuiRLD2rV854AV8gfMYchQhamxcp4X16phnyzdPLR",
+    6: "QmdrhhjL2R49SYdwdPF3eEVU1GHDSdJ4Ykya3jvt7TdRTu",
+    7: "QmXiD3T5erwNAZ5yFYpz3AA2h6FXfPMt7XzKCDrKwJSMDX",
+    8: "QmW4jpn5EnjQx6aCbSAodEhh6BW2UqvZSGvubu2tv4UWCn"
 };
 
 export const TOKENS = [
